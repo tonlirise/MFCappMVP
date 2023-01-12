924d53060f03c8d238da9b58d88e7e7e2b65e7af
tonlirise <tonlirise@gmail.com>
2023-01-12 10:25:10 +0700

    Fix unit test

--- MFCappMVP/Resource.h
+++ MFCappMVP/Resource.h
@@ -1,21 +1,30 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
-// Используется в MFCappMVP.rc
+// Используется MFCappMVP.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_MFCAPPMVP_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPMVP_DIALOG            102
+#define IDD_DIALOG_PERSON_LIST          102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST_ALL                    1000
+#define IDC_EDIT_NAME                   1001
+#define IDC_EDIT_AGE                    1002
+#define IDC_EDIT_ADDRESS                1003
+#define IDC_BUTTON_SAVE                 1004
+#define IDC_BUTTON_UPDATE               1005
+#define IDC_STATIC_NAME                 1006
+#define IDC_STATIC_AGE                  1007
+#define IDC_STATIC_ADDRESS              1008
 
-// Следующие значения по умолчанию для новых объектов
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- UnitTests/PersonListTest.cpp
+++ UnitTests/PersonListTest.cpp
@@ -1,8 +1,54 @@
 #include "StdAfx.h"
 #include <gtest/gtest.h>
 
+#include "..\MFCappMVP\PersonListPresenterImpl.h"
+
 namespace Middle
 {
+	class CPersonListViewTestImpl : public CPersonListView
+	{
+	public:
+		CPersonListViewTestImpl();
+		~CPersonListViewTestImpl();
+
+		void SetUserListBox(std::map<long, CPerson> data) {};
+		void SetName(std::string value) {};
+		std::string GetName() {};
+		long getSelectedUserID() {};
+
+		void SetAge(int value) {};
+		int GetAge() {};
+
+		void SetAddress(std::string value) {};
+		std::string GetAddress() {};
+
+		void SetPresenter(CPersonListPresenter* presenter) {};
+
+	};
+
+	CPersonListViewTestImpl::CPersonListViewTestImpl(){}
+
+	CPersonListViewTestImpl::~CPersonListViewTestImpl(){}
+
+	class CPersonListRepositoryTestIml : public CPersonListRepository
+	{
+	public:
+		CPersonListRepositoryTestIml();
+		~CPersonListRepositoryTestIml();
+
+		void SaveUser(CPerson user) {};
+		CPerson GetUser(long id) {};
+		std::map<long, CPerson> GetAllUsers() {};
+		CPerson FindbyName(std::string name) {};
+		CPerson FindbyID(long id) {};
+		void UpdateUser(CPerson user) {};
+
+	};
+
+	CPersonListRepositoryTestIml::CPersonListRepositoryTestIml(){}
+
+	CPersonListRepositoryTestIml::~CPersonListRepositoryTestIml(){}
+
 	class CPersonListTest : public testing::Test
 	{
 	public:
@@ -12,6 +58,12 @@ namespace Middle
 
 	TEST_F(CPersonListTest, AddButtonClickTest)
 	{
+		CPersonListViewTestImpl testView;
+		CPersonListRepositoryTestIml testRepository;
+
+		CPersonListPresenterImpl m_PersonListPresenter
+			= CPersonListPresenterImpl(&testView, &testRepository);
+
 		EXPECT_EQ(0, 0);
 	}
 }
