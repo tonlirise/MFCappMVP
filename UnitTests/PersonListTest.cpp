f5209b7a08058e38daad31635579f23b240b881f
tonlirise <tonlirise@gmail.com>
2023-01-19 14:00:05 +0700

    Fix unit test

--- UnitTests/PersonListTest.cpp
+++ UnitTests/PersonListTest.cpp
@@ -10,10 +10,6 @@ namespace Middle
 	public:
 		int m_nCountSetUserListBox{ 0 };
 
-		int m_nCountNumGetName{ 0 };
-		int m_nCountNumGetAge{ 0 };
-		int m_nCountNumGetAddress{ 0 };
-
 		std::string m_sTestName;
 		int m_nTestAge{-1};
 		std::string m_sTestAddress;
@@ -23,14 +19,14 @@ namespace Middle
 
 		void SetUserListBox(std::map<long, CPerson> data) { m_nCountSetUserListBox++; };
 		void SetName(std::string value) {};
-		std::string GetName() { m_nCountNumGetName++; return m_sTestName; };
+		std::string GetName() { return m_sTestName; };
 		long getSelectedUserID() { return -1; };
 
 		void SetAge(int value) {};
-		int GetAge() { m_nCountNumGetAge++; return m_nTestAge; };
+		int GetAge() { return m_nTestAge; };
 
 		void SetAddress(std::string value) {};
-		std::string GetAddress() { m_nCountNumGetAddress++; return m_sTestAddress; };
+		std::string GetAddress() { return m_sTestAddress; };
 
 		void SetPresenter(IPersonListPresenter* presenter) {};
 
@@ -42,10 +38,6 @@ namespace Middle
 	public:
 		int m_nCountSaveUser{ 0 };
 		int m_nCountUpdateUser{ 0 };
-
-		std::string m_sFindedTestName;
-		int m_nFindedTestAge{ -1 };
-		std::string m_sFindedTestAddress;
 
 		CPersonListRepositoryTestIml() {};
 		~CPersonListRepositoryTestIml() {};
@@ -75,17 +67,14 @@ namespace Middle
 		CPersonListPresenterImpl personListPresenterTest
 			= CPersonListPresenterImpl(&testView, &testRepository);
 
-		testView.m_sTestName = "Ivan";
-		testView.m_nTestAge = 35;
-		testView.m_sTestAddress = "Lenina street";
+		CPerson uiPerson;
+		uiPerson.SetName("Ivan");
+		uiPerson.SetAge(35);
+		uiPerson.SetAddress("Ivan");
 
-		personListPresenterTest.SaveUser();
+		personListPresenterTest.SaveUser(uiPerson);
 
-		EXPECT_EQ(1, testView.m_nCountNumGetName);
-		EXPECT_EQ(1, testView.m_nCountNumGetAge);
-		EXPECT_EQ(1, testView.m_nCountNumGetAddress);
 		EXPECT_EQ(1, testView.m_nCountSetUserListBox);
-
 		EXPECT_EQ(1, testRepository.m_nCountSaveUser);
 	}
 
@@ -97,13 +86,12 @@ namespace Middle
 		CPersonListPresenterImpl personListPresenterTest
 			= CPersonListPresenterImpl(&testView, &testRepository);
 
-		personListPresenterTest.SaveUser();
+		CPerson uiPerson;
+		personListPresenterTest.SaveUser(uiPerson);
+		uiPerson.SetAge(35);
+		uiPerson.SetAddress("Ivan");
 
-		EXPECT_EQ(1, testView.m_nCountNumGetName);
-		EXPECT_EQ(1, testView.m_nCountNumGetAge);
-		EXPECT_EQ(1, testView.m_nCountNumGetAddress);
 		EXPECT_EQ(1, testView.m_nCountSetUserListBox);
-
 		EXPECT_EQ(0, testRepository.m_nCountSaveUser);
 	}
 }
