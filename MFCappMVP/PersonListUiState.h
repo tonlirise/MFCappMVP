d8dc0ec49ed44cf4c86a998adecce172bb2df597
tonlirise <tonlirise@gmail.com>
2023-01-25 09:55:57 +0700

    Update state with ptr value

--- MFCappMVP/PersonListUiState.h
+++ MFCappMVP/PersonListUiState.h
@@ -8,54 +8,46 @@
 
 class CPersonListUiState
 {
-	bool m_bChangedPersonList{ false };
-	std::map<long, CPerson> m_mapPersonsList;
+	std::shared_ptr<std::map<long, CPerson>> m_ptrMapPersonsList;
+	
+	std::shared_ptr<std::string> m_ptrName;
 
-	bool m_bChangedName{ false };
-	std::string m_sName;
+	std::shared_ptr<long> m_ptrAge;
 
-	bool m_bChangedAge{ false };
-	long m_nAge{ -1 };
-
-	bool m_bChangedAddress{ false };
-	std::string m_sAddress;
+	std::shared_ptr<std::string> m_ptrAddress;
 
 public:
 	CPersonListUiState() = default;
 
-	void SetPersonList(std::map<long, CPerson> mapPersonsList)
+	void SetPersonList(std::map<long, CPerson>* mapPersonsList)
 	{
-		m_bChangedPersonList = true;
-		m_mapPersonsList = mapPersonsList;
+		m_ptrMapPersonsList = std::make_shared<std::map<long, CPerson>>(*mapPersonsList);
 	}
 
-	void SetName(std::string sName)
+	void SetName(std::string* sName)
 	{
-		m_bChangedName = true;
-		m_sName = sName;
+		m_ptrName = std::make_shared<std::string>(*sName);
 	}
 
 	void SetAge(long nAge)
 	{
-		m_bChangedAge = true;
-		m_nAge = nAge;
+		m_ptrAge = std::make_shared<long>(nAge);
 	}
 
-	void SetAddress(std::string sAddress)
+	void SetAddress(std::string* sAddress)
 	{
-		m_bChangedAddress = true;
-		m_sAddress = sAddress;
+		m_ptrAddress = std::make_shared<std::string>(*sAddress);
 	}
 
-	void Apply(CListBox* pUserListBox, 
+	void Apply(CListBox* pUserListBox,
 		CEdit* pNameEditBox,
-		CEdit* pAgeEditBox, 
+		CEdit* pAgeEditBox,
 		CEdit* pAddressEditBox)
 	{
-		if (pUserListBox && m_bChangedPersonList)
+		if (pUserListBox && m_ptrMapPersonsList)
 		{
 			pUserListBox->ResetContent();
-			for each (auto i in m_mapPersonsList)
+			for each (auto i in *m_ptrMapPersonsList)
 			{
 				std::string val = i.second.GetName() + " | " + i.second.GetAddress();
 				std::wstring path_wstr(val.begin(), val.end());
@@ -63,31 +55,31 @@ public:
 			}
 		}
 
-		if (pNameEditBox && m_bChangedName)
+		if (pNameEditBox && m_ptrName)
 		{
-			std::wstring wstr(m_sName.begin(), m_sName.end());
+			std::wstring wstr(m_ptrName->begin(), m_ptrName->end());
 			pNameEditBox->SetWindowText(wstr.c_str());
 		}
 
-		if (pAgeEditBox && m_bChangedAge)
+		if (pAgeEditBox && m_ptrAge)
 		{
 			CString str;
-			str.Format(_T("%d"), m_nAge);
+			str.Format(_T("%d"), *m_ptrAge);
 			pAgeEditBox->SetWindowText(str);
 		}
 
-		if (pAddressEditBox && m_bChangedAddress)
+		if (pAddressEditBox && m_ptrAddress)
 		{
-			std::wstring wstr(m_sAddress.begin(), m_sAddress.end());
+			std::wstring wstr(m_ptrAddress->begin(), m_ptrAddress->end());
 			pAddressEditBox->SetWindowText(wstr.c_str());
 		}
-	}
+	} 
 
-	void ClearAllChangedMask()
+	void ResetAllPtr()
 	{
-		m_bChangedPersonList = false;
-		m_bChangedName = false;
-		m_bChangedAge = false;
-		m_bChangedAddress = false;
+		m_ptrMapPersonsList = nullptr;
+		m_ptrName = nullptr;
+		m_ptrAge = nullptr;
+		m_ptrAddress = nullptr;
 	}
 };
--- MFCappMVP/PersonListViewModel.cpp
+++ MFCappMVP/PersonListViewModel.cpp
@@ -22,7 +22,7 @@ void CPersonListViewModel::UpdateUser(CPerson objPerson)
 	m_IPersonListRepository->UpdateUser(objPerson);
 
 	auto allUsers = m_IPersonListRepository->GetAllUsers();
-	m_uiCurrState.SetPersonList(allUsers);
+	m_uiCurrState.SetPersonList(&allUsers);
 	PushCurrStateToUI();
 }
 
@@ -31,7 +31,7 @@ void CPersonListViewModel::SaveUser(CPerson objPerson)
 	m_IPersonListRepository->SaveUser(objPerson);
 
 	auto allUsers = m_IPersonListRepository->GetAllUsers();
-	m_uiCurrState.SetPersonList(allUsers);
+	m_uiCurrState.SetPersonList(&allUsers);
 	PushCurrStateToUI();
 }
 
@@ -40,14 +40,14 @@ void CPersonListViewModel::SelectItem(int nSelectedUserID)
 	CPerson* pUser = m_IPersonListRepository->SelectItem(nSelectedUserID);
 	if (!pUser) return;
 
-	m_uiCurrState.SetName(pUser->GetName());
+	m_uiCurrState.SetName(&pUser->GetName());
 	m_uiCurrState.SetAge(pUser->GetAge());
-	m_uiCurrState.SetAddress(pUser->GetAddress());
+	m_uiCurrState.SetAddress(&pUser->GetAddress());
 	PushCurrStateToUI();
 }
 
 void CPersonListViewModel::PushCurrStateToUI()
 {
 	m_pObsUiState->UpdateValue(&m_uiCurrState);
-	m_uiCurrState.ClearAllChangedMask();
+	m_uiCurrState.ResetAllPtr();
 }
